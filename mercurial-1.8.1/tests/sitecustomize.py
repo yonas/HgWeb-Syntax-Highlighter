5f3eb251ea2652d9784e89b9e9cc449f113fba22
Yonas <yonas@yonas-VirtualBox.(none)>
2011-11-17 02:32:41 -0500

    Remove mercurial v1.8.1 in favour of v2.0

--- mercurial-1.8.1/tests/sitecustomize.py
+++
@@ -1,6 +0,0 @@
-try:
-    import coverage
-    if hasattr(coverage, 'process_startup'):
-        coverage.process_startup()
-except ImportError:
-    pass
--- mercurial-1.8.1/tests/test-hghave.t
+++
@@ -1,3 +0,0 @@
-Testing that hghave does not crash when checking features
-
-  $ "$TESTDIR/hghave" --test-features 2>/dev/null
